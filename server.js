a221e79d1e5e7335f5a0daa28d0979ddd70f0332
codevil <codevil0209@gmail.com>
2025-02-15 09:16:34 +0530

    updated server js code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDb =require('./InflosoAI/Config/db');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { json } = require('body-parser');
+
+//defining all the environment configurations and database configurations in our main file
+dotenv.config();
+connectDb();
+
+//creating an object of Express
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(json());
+
+app.use('/api/auth', require('./InflosoAI/Router/UserAuthRouter'))
+//running our express server on the declared port in environment
+app.listen(process.env.PORT, ()=>{
+console.log(`your InflosoAI server is running on ${process.env.PORT}`);
+})
